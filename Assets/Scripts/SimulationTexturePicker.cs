71c4fd37f8c6e57c1c63e2dda67052df022b7ede
Evan Lang <etlang@protonmail.com>
2026-01-31 14:03:25 -0700

    Added bespoke analysis capabilities to SimulationTexturePicker.

--- Assets/Scripts/SimulationTexturePicker.cs
+++ Assets/Scripts/SimulationTexturePicker.cs
@@ -1,11 +1,12 @@
+using System;
 using UnityEngine;
 
 [RequireComponent(typeof(Renderer))]
-public class SimulationTexturePicker : MonoBehaviour {
+public class SimulationTexturePicker : PhotonerComponent {
+    [SerializeField] float brightnessThreshold = 1;
+    [SerializeField] float varianceThreshold = 1;
+
     public enum TextureType {
-        Photons_Forward,
-        Photons_Raw,
-        Camera_Accum,
         HDR,
         Variance,
         Importance,
@@ -15,15 +16,41 @@ public class SimulationTexturePicker : MonoBehaviour {
         Transmissibility,
         NormalRoughness,
         QuadTree,
+        AnalysisA,
     }
 
     [SerializeField] public Simulation simulation;
     //[SerializeField] private AIAccelerator aiAccelerator;
     [SerializeField] public TextureType type = TextureType.HDR;
 
-    void OnDisable()
+    int _analysisAKernelId;
+
+    ComputeShader _analysisShader;
+    RenderTexture _analysisTarget;
+    int _selectedAnalysisKernel = -1;
+
+    void Awake()
+    {
+        _analysisShader = (ComputeShader)Resources.Load("Analysis");
+        _analysisAKernelId = _analysisShader.FindKernel("AnalysisA");
+    }
+
+    void OnEnable()
+    {
+        if(simulation)
+        {
+            simulation.OnStep += OnSimulationUpdated;
+        }
+    }
+
+    protected override void OnDisable()
     {
         GetComponent<Renderer>().material.SetTexture("_MainTex", null);    
+
+        if(simulation)
+        {
+            simulation.OnStep -= OnSimulationUpdated;
+        }
     }
 
     void LateUpdate()
@@ -34,24 +61,15 @@ public class SimulationTexturePicker : MonoBehaviour {
         Texture value = null;
 
         switch(type) {
-        // case TextureType.Photons_Forward:
-        //     value = simulation?.SimulationForwardHDR;
-        //     break;
-        // case TextureType.Camera_Accum:
-        //     value = simulation?.SimulationBackwardAccumulated;
-        //     break;
-        // case TextureType.Photons_Raw:
-        //     value = simulation?.SimulationPhotonsRaw;
-        //     break;
         case TextureType.HDR:
             value = simulation?.SimulationOutputHDR;
             break;
-        case TextureType.AI_ToneMapped:
-            //value = aiAccelerator?.ToneMappedOutputTexture;
-            break;
-        case TextureType.AI_HDR:
-            //value = aiAccelerator?.HDROutputTexture;
-            break;
+        // case TextureType.AI_ToneMapped:
+        //     //value = aiAccelerator?.ToneMappedOutputTexture;
+        //     break;
+        // case TextureType.AI_HDR:
+        //     //value = aiAccelerator?.HDROutputTexture;
+        //     break;
         case TextureType.Albedo:
             value = simulation?.GBuffer.AlbedoAlpha;
             break;
@@ -70,10 +88,47 @@ public class SimulationTexturePicker : MonoBehaviour {
         case TextureType.Importance:
             value = simulation?.ImportanceMap;
             break;
+        case TextureType.AnalysisA:
+            ConfigureAnalysisA();
+            value = _analysisTarget;
+            break;
+        }
+
+        if(value != _analysisTarget) {
+            _selectedAnalysisKernel = -1;
         }
 
         if(value != null) {
             renderer.material.SetTexture("_MainTex", value);
         }
+    }
+
+    void ConfigureAnalysisA()
+    {
+        if(_selectedAnalysisKernel == _analysisAKernelId) { return; }
+
+        _selectedAnalysisKernel = _analysisAKernelId;
+        _analysisTarget = this.CreateRWTexture(simulation.width, simulation.height, RenderTextureFormat.ARGBFloat);
+    }
+
+    private void OnSimulationUpdated(int frameCount)
+    {
+        if(_selectedAnalysisKernel == -1) { return; }
+
+        _analysisShader.RunKernel(_selectedAnalysisKernel, _analysisTarget.width, _analysisTarget.height,
+            ("_out_analysis", _analysisTarget),
+            ("_in_albedo", simulation.GBuffer.AlbedoAlpha),
+            ("_in_transmissibility", simulation.GBuffer.Transmissibility),
+            ("_in_normal_roughness", simulation.GBuffer.NormalRoughness),
+            ("_in_hdr_forward_a", simulation.TracerA.EarlyRadianceForImportanceSampling),
+            ("_in_hdr_forward_b", simulation.TracerB.EarlyRadianceForImportanceSampling),
+            ("_in_hdr_a", simulation.TracerA.TracerOutput),
+            ("_in_hdr_b", simulation.TracerB.TracerOutput),
+            ("_in_hdr_final", simulation.SimulationOutputHDR),
+            ("_in_importance", simulation.ImportanceMap),
+            ("_in_variance", simulation.VarianceMap),
+            ("_brightness_threshold", brightnessThreshold),
+            ("_variance_threshold", varianceThreshold)
+        );
     }
 }
